5eb18fec4fe35c14be819e6004e0ed6cdd6c91cd
easonblack <eason820720@163.com>
2018-01-12 16:57:02 +0800

    up

---
+++ basic/java-basic/logger/src/main/java/com/App.java
@@ -0,0 +1,14 @@
+package com;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class App {
+    public static void main(String[] args){
+      System.out.println("xxxxxxxxxxxxxxxxxxxxxxxxxxx");
+      Logger logger = LoggerFactory.getLogger(App.class);
+      logger.info("Hello World");
+      logger.info("Hello World");
+      logger.info("Hello World");
+    }
+}
---
+++ basic/java-basic/mvn-exec/src/main/java/com/App.java
@@ -0,0 +1,8 @@
+package com;
+
+
+public class App {
+    public static void main(String[] args){
+      System.out.println("This is my first program in java");
+    }
+}
